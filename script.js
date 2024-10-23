893469d504189fe2bc55d96c467e671eaba8d3b9
Timothy <128176649+Timothy-P@users.noreply.github.com>
2024-10-23 11:01:22 -0500

    Just putting up my files.
    I don't have very much done, or working. Use as desired, as it isn't perfect.

---
+++ script.js
@@ -0,0 +1,79 @@
+window.addEventListener("DOMContentLoaded", function () {
+    this.alert("Content loaded.");
+});
+function Click(event) {
+    const element = event.target;
+    let highlighted = {};
+    highlightElm(element);
+}
+function addCode(ElmId, ElmType) {
+    document.getElementById(ElmId).innerHTML += ElmType;
+}
+
+// Checkers functions...
+function highlightElm(element) {
+    alert(
+`Element received: ${element.id}
+Element row: ${element.attributes.row.value}
+Element colomn: ${element.attributes.colomn.value}`
+    )
+    var UpperLeft1 = element.attributes.row.value - 1;
+    if (UpperLeft1 > 8 || UpperLeft1 < 1) {
+        UpperLeft1 = "N/A";
+    }
+    var UpperLeft2 = element.attributes.colomn.value - 1;
+    if (UpperLeft2 > 8 || UpperLeft1 < 1) {
+        UpperLeft2 = "N/A";
+    }
+    if (UpperLeft1 != "N/A" && UpperLeft2 != "N/A") {
+        var UpperLeft = `${UpperLeft1}-${UpperLeft2}`;
+    }
+    var LowerRight1 = element.attributes.row.value +1;
+    if (LowerRight1 > 8 || LowerRight1 < 1) {
+        LowerRight1 = "N/A";
+    }
+    let LowerLeft2 = element.attributes.colomn.value - 1;
+    if (LowerLeft2 > 8 || LowerLeft1 < 1) {
+        LowerLeft2 = "N/A";
+    }
+    if (LowerRight1 != "N/A" && LowerRight2 != "N/A") {
+        var LowerRight = `${LowerRight1}-${LowerRight2}`;
+    }
+
+    var LowerLeft1 = element.attributes.row.value - 1;
+    if (LowerLeft1 > 8 || LowerLeft1 < 1) {
+        LowerLeft1 = "N/A";
+    }
+    var LowerLeft2 = element.attributes.colomn.value - 1;
+    if (LowerLeft2 > 8 || LowerLeft1 < 1) {
+        LowerLeft2 = "N/A";
+    }
+    if (LowerLeft1 != "N/A" && LowerLeft2 != "N/A") {
+        var LowerLeft = `${LowerLeft1}-${LowerLeft2}`;
+    }
+
+    if (UpperRight1 > 8 || UpperRight1 < 1) {
+        UpperRight1 = "N/A";
+    }
+    var UpperRight2 = element.attributes.colomn.value - 1;
+    if (UpperRight2 > 8 || UpperRight1 < 1) {
+        UpperRight2 = "N/A";
+    }
+    if (UpperRight1 != "N/A" && UpperRight2 != "N/A") {
+        var UpperRight = `${UpperRight1}-${UpperRight2}`;
+    }
+    alert(
+`Upper left: ${document.getElementById(UpperLeft).id}
+    Upper left row: ${document.getElementById(UpperLeft).attributes.row.value}
+    Upper left colomn: ${document.getElementById(UpperLeft).attributes.colomn.value}
+Upper right: ${document.getElementById(UpperRight).id}
+    Upper right row: ${document.getElementById(UpperRight).attributes.row.value}
+    Upper right colomn: ${document.getElementById(UpperRight).attributes.colomn.value}
+Lower left: ${document.getElementById(LowerLeft).id}
+    Lower left row: ${document.getElementById(LowerLeft).attributes.row.value}
+    Lower left colomn: ${document.getElementById(LowerLeft).attributes.colomn.value}
+Lower right: ${document.getElementById(LowerRight).id}
+    Lower right row: ${document.getElementById(LowerRight).attributes.row.value}
+    Lower right colomn: ${document.getElementById(LowerRight).attributes.colomn.value}
+    `)
+}
